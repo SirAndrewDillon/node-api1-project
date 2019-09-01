ebdc7d9140284ce86c5acc5d24ed51fca08f8832
andrew dillon <majik895@gmail.com>
2019-09-01 19:01:49 -0400

    done with backend crud attempting cra stretch

--- index.js
+++ index.js
@@ -1,1 +1,124 @@
-// implement your API here
+//Requires the NPM Express module
+const express = require('express')
+//Creates an Express application using the express module
+const server = express()
+//Makes the database available to the server
+const Users = require('./data/db.js')
+
+server.use(express.json());
+
+// server sanity check
+server.get('/', (req, res) => {
+    res.send('<h1>Welcome to the Jungle</h1>')
+})
+
+// GET users end points
+//Returns an array of all the user objects contained in the database.
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: 'The users information could not be retrieved.',
+      })
+    })
+})
+
+//Returns the user object with the specified `id`.
+server.get('/api/users/:id', (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: 'The user information could not be retrieved.' });
+    });
+});
+
+//Creates a user using the information sent inside the `request body`
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    Users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            'There was an error while saving the user to the database',
+        });
+      });
+  }
+});
+
+//Removes the user with the specified `id` and returns the deleted user.
+server.delete('/api/users/:id', (req, res) => {
+  Users.remove(req.params.id)
+    .then(count => {
+      if (count && count > 0) {
+        res.status(200).json({
+          message: 'the user was deleted.',
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: 'The user could not be removed' });
+    });
+});
+
+
+//Updates the user with the specified `id` using data from the `request body`. Returns the modified document
+server.put('/api/users/:id', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res
+            .status(404)
+            .json({
+              message: 'The user with the specified ID does not exist.',
+            });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: 'The user information could not be modified.',
+        });
+      });
+  }
+});
+
+
+
+
+//Once the server is fully configured you can have it listen for connections
+//The callback function is passed as the second argument will run once the server starts
+server.listen(1234, () => console.log("Chilling and having a cold one on port 1234"))
